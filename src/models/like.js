e562cdc2916afaf2a2c442517cbfe7fb9237ca67
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2023-09-15 20:03:21 +0530

    Added like model and basic user model

---
+++ src/models/like.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+/* We create a like model. Like will be done on the comment or like will be done on the tweet. So we create
+a extandable model which is used for both purpose.
+*/
+const likeSchema = mongoose.Schema({
+    onModel: {
+        type: String,
+        require: true,
+        enum: ['Tweet', 'Comment']
+    },
+    likeable: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        refPath: 'onModel'
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+}, {timestamps:true});
+
+const Like = mongoose.model('Like', likeSchema);
+export default Like;
